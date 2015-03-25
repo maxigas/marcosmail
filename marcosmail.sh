7af95c43439ff8e4af1fb32771e976d6efee821b
maxigas <maxigas@anargeek.net>
2015-03-25 22:25:28 +0100

    put email address to variable

--- marcosmail.sh
+++ marcosmail.sh
@@ -4,6 +4,7 @@
 # apt-get install mailutils
 
 HOSTNAME=`hostname`
+EMAIL="admin@example.com"
 IP=`ifconfig eth0|grep inet|awk '{print $2}'|sed s/addr://`
 if [ -f /tmp/ip ]
 then OLDIP=`cat /tmp/ip`
@@ -11,7 +12,7 @@ else OLDIP=0.0.0.0
 fi
 
 if ! [ "$OLDIP" = "$IP" ]
-then mail -s "$HOSTNAME changed IP from $OLDIP to $IP." maxigas@anargeek.net
+then mail -s "$HOSTNAME changed IP from $OLDIP to $IP." $EMAIL
 fi
 
 echo $IP > /tmp/ip

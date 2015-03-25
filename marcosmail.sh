b784c151a6b28b47e3c5f33768035e98e415abf3
maxigas <maxigas@anargeek.net>
2015-03-25 22:20:40 +0100

    init2

---
+++ marcosmail.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Requirements:
+# apt-get install mailutils
+
+HOSTNAME=`hostname`
+IP=`ifconfig eth0|grep inet|awk '{print $2}'|sed s/addr://`
+if [ -f /tmp/ip ]
+then OLDIP=`cat /tmp/ip`
+else OLDIP=0.0.0.0
+fi
+
+if ! [ $OLDIP = $IP ]
+then mail -s "$HOSTNAME changed IP from $OLDIP to $IP." maxigas@anargeek.net
+fi
+
+echo $IP > /tmp/ip
+
+
